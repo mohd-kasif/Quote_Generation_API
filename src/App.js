245a74e2ddabf223a2ff2725d77a8213aae4fb6a
kashif <kashifmd123@gmail.com>
2022-02-26 21:09:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
 function App() {
+  const [list, setList] = useState([]);
+
+  const fetchQuote = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      setList(data);
+    } catch (e) {
+      throw new Error();
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card style={{ backgroundColor: "#1E1E1E" }}>
+        <CardContent>
+          <Typography
+            sx={{ mb: 1.5 }}
+            color="text.secondary"
+            style={{ color: "white", fontFamily: "cursive" }}
+            variant="body2"
+          >
+            {list.length !== 0 ? (
+              list.content
+            ) : (
+              <p style={{ align: "center" }}>Click to generate a quote</p>
+            )}
+          </Typography>
+          <Typography
+            sx={{ fontSize: 14, fontWeight: "bold" }}
+            style={{
+              color: "white",
+              alignItems: "right",
+              fontFamily: "cursive",
+            }}
+            variant="body1"
+            align="right"
+          >
+            <br />
+            {list.author}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="text"
+            size="small"
+            onClick={fetchQuote}
+            align="center"
+            style={{ color: "#89C1EF", backgroundColor: "#14273A" }}
+          >
+            Generate Quote
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
